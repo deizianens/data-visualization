a2c92bbb92db9bcef6f125abe9bf2dacf29a8791
Deiziane <deiziane_ns@hotmail.com>
2018-10-24 20:34:26 -0300

    donut working

--- TP2/js/donut-chart.js
+++ TP2/js/donut-chart.js
@@ -11,21 +11,29 @@ d3_queue
     .await(ready);
 
 function ready(error, data) {
+    var idx = -1;
+    // find country index on array
+    for(var i = 0; i<data.length; i++){
+        if(data[i].Country == country){
+            idx = i;
+        }
+    }
+    // console.log(idx);
     // var pieData = {};
-    var columns = ["Economy", "Family", "Health" , "Freedom", "Trust", "Generosity", "Dystopia"];
-    var stack = d3.stack().keys(columns);
-    var layers = stack(data).map(function (layer){ return layer.map(function(e, i) {
-        return { country: e.data.Country,
-                 contribution: layer.key, 
-                 score: e.data.Score,
-                 rank: e.data.Rank,
-                 region: e.data.Region,
-                 cores:["#ED254E","#8FF7A7","#FFEF60",'#51BBFE','#25CED1','#4E5379','#B03FC1']               
-        };
-    });
-    });
+    // var columns = console.log(data[0].Country);
+    // var stack = d3.stack().keys(columns);
+    // var layers = stack(data).map(function (layer){ return layer.map(function(e, i) {
+    //     return { country: e.data.Country,
+    //              contribution: layer.key, 
+    //              score: e.data.Score,
+    //              rank: e.data.Rank,
+    //              region: e.data.Region,
+    //              color:["#ED254E","#8FF7A7","#FFEF60",'#51BBFE','#25CED1','#4E5379','#B03FC1']               
+    //     };
+    // });
+    // });
 
-    console.log(layers);
+    // console.log(layers[1][0]);
     const pieData = [
         { name: "Economia", value: 17, color: '#ED254E'},
         { name: "Família", value: 13, color: '#8FF7A7' },
@@ -36,24 +44,47 @@ function ready(error, data) {
         { name: "Distopia", value: 6, color: '#B03FC1' },
     ];
 
-    bakeDonut(pieData);
+    bakeDonut(data[idx]);
+    // bakeDonut(pieData);
 }
 
 function bakeDonut(d) {
+    // var columns = ["Economy", "Family", "Health" , "Freedom", "Trust", "Generosity", "Dystopia"];
+    var colors = ["#ED254E","#8FF7A7","#FFEF60",'#51BBFE','#25CED1','#4E5379','#B03FC1'];
+    const pieData = [
+        { name: "Economia", value: d.Economy, color: '#ED254E'},
+        { name: "Família", value: d.Family, color: '#8FF7A7' },
+        { name: "Vida Saudável", value: d.Health, color: '#FFEF60' },
+        { name: "Liberdade", value: d.Freedom, color: '#51BBFE' },
+        { name: "Confiança", value: d.Trust, color: '#25CED1' },
+        { name: "Generosidade", value: d.Generosity, color: '#4E5379' },
+        { name: "Distopia", value: d.Dystopia, color: '#B03FC1' },
+    ];
+    // const pieData = [
+    //     { name: "Economia", value: (d.Economy*100/d.Score).toFixed(2), color: '#ED254E'},
+    //     { name: "Família", value: (d.Family*100/d.Score).toFixed(2), color: '#8FF7A7' },
+    //     { name: "Vida Saudável", value: (d.Health*100/d.Score).toFixed(2), color: '#FFEF60' },
+    //     { name: "Liberdade", value: (d.Freedom*100/d.Score).toFixed(2), color: '#51BBFE' },
+    //     { name: "Confiança", value: (d.Trust*100/d.Score).toFixed(2), color: '#25CED1' },
+    //     { name: "Generosidade", value: (d.Generosity*100/d.Score).toFixed(2), color: '#4E5379' },
+    //     { name: "Distopia", value: (d.Dystopia*100/d.Score).toFixed(2), color: '#B03FC1' },
+    // ];
+    // var values = [d.Economy, d.Family, d.Health, d.Freedom, d.Trust, d.Generosity, d.Dystopia];
+    // console.log(values);
     let activeSegment;
-    const data = d.sort((a, b) => b['value'] - a['value']),
+    const data = pieData.sort( (a, b) => b['value'] - a['value']),
         viewWidth = 600,
         viewHeight = 400,
         svgWidth = viewHeight,
         svgHeight = viewHeight,
         thickness = 60,
         colorArray = data.map(k => k.color),
-        el = d3.select('body'),
+        // el = d3.select('body'),
         radius = Math.min(svgWidth, svgHeight) / 2,
         color = d3.scaleOrdinal()
             .range(colorArray);
 
-    const max = d3.max(data, (maxData) => maxData.value);
+    const max = d3.max(pieData, (maxData) => maxData.value);
 
     const svg = d3
         .select(".ddonut")
@@ -79,9 +110,11 @@ function bakeDonut(d) {
     const arcHover = d3.arc()
         .innerRadius(radius - (thickness + 5))
         .outerRadius(radius + 8);
-
+    
+    var i = 0;
+    console.log(data);
     const pie = d3.pie()
-        .value(function (pieData) { return pieData.value; })
+        .value(function(pieData) { return pieData.value; })
         .sort(null);
 
 
