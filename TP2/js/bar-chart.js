6d4a0785a402bf01eb6df9612b8f626ad35c4161
Deiziane <deiziane_ns@hotmail.com>
2018-10-27 21:18:30 -0300

    fix country-continent file to match

--- TP2/js/bar-chart.js
+++ TP2/js/bar-chart.js
@@ -1,12 +1,21 @@
 var year = 2015;
 var countryMapping;
 
-/**
- * Loads the countries mapping.
- */
-d3.csv("country-continent.csv", function (er, d) {
-    countryMapping = d;
-});
+function findCountryContinent(country_name){
+    for (var i = 0, len = countryMapping.length; i < len; i++) {
+        if (countryMapping[i]["country"] === country_name){
+            if(countryMapping[i]["continent"] == "Americas"){
+                if(countryMapping[i]["sub_region"] == "Northern America"){
+                    return countryMapping[i]["sub_region"];
+                }
+                else{
+                    return countryMapping[i]["continent"];
+                }
+            } 
+            return countryMapping[i]["continent"];
+        }
+    }
+}
 
 // target the single container and include one div for data-viz
 const container = d3.select(".container-chart");
@@ -44,49 +53,61 @@ d3_queue
         d3.csv,
         "./data/world-happiness-report-" + year + "-kaggle.csv"
     )
+    .defer(
+        d3.csv,
+        "country-continent.csv"
+    )
     .await(ready);
 
-function ready(data) {
+function ready(error, data, c) {
+    if (error) throw error;
+    // console.log(data);
+    countryMapping = c;
+   
     var cat = new Array(6).fill(0);
     var val = new Array(6).fill(0);
     for(var i = 0; i < data.length; i++){
         if(findCountryContinent(data[i].Country) == "Africa"){
             cat[0] += 1; 
-            val[0] += data[i].Score; 
+            val[0] += parseInt(data[i].Score); 
         }
         else if(findCountryContinent(data[i].Country) == "Americas"){
             cat[1] += 1; 
-            val[1] += data[i].Score;
+            val[1] += parseInt(data[i].Score);
+            console.log(data[i].Country);
         }
         else if(findCountryContinent(data[i].Country) == "Northern America"){
             cat[2] += 1; 
-            val[2] += data[i].Score;
+            val[2] += parseInt(data[i].Score);
         }
         else if(findCountryContinent(data[i].Country) == "Europe"){
             cat[3] += 1; 
-            val[3] += data[i].Score;
+            val[3] += parseInt(data[i].Score);
         }
         else if(findCountryContinent(data[i].Country) == "Oceania"){
             cat[4] += 1; 
-            val[4] += data[i].Score;
+            val[4] += parseInt(data[i].Score);
         }
         else if(findCountryContinent(data[i].Country) == "Asia"){
             cat[5] += 1; 
-            val[5] += data[i].Score;
+            val[5] += parseInt(data[i].Score);
         }
         else {
-            console.log("Continent not found! :(")
+            console.log("Continent not found! :( "+data[i].Country);
         }
     }
     
-    const dataLicenses = [
+    const dataValues = [
         { category: "Africa", value: parseFloat(val[0]/cat[0]).toFixed(1)},
-        { category: "Americas", value: parseFloat(val[1]/cat[1]).toFixed(1)},
+        { category: "Latin America", value: parseFloat(val[1]/cat[1]).toFixed(1)},
         { category: "Northern America", value: parseFloat(val[2]/cat[2]).toFixed(1)},
         { category: "Europe", value: parseFloat(val[3]/cat[3]).toFixed(1)},
         { category: "Oceania", value: parseFloat(val[4]/cat[4]).toFixed(1)},
         { category: "Asia", value: parseFloat(val[5]/cat[5]).toFixed(1)}
     ];
+    
+    dataLicenses = dataValues.sort( (a, b) => b['value'] - a['value'])
+    console.log(dataLicenses);
 
     // include a section for the specific visualization
     const licenses = container
@@ -95,11 +116,15 @@ function ready(data) {
     // include introductory heading and paragraph
     licenses
         .append("h2")
-        .text("Number of licenses");
+        .text("Felicidade por Continente");
 
     licenses
         .append("p")
-        .text("Comparison between prevalent sport licenses in 2015 and hunting licenses");
+        .text("Comparação do nível de felicidade entre continentes");
+    
+    licenses
+        .append("span")
+        .text("(América foi subdividida entre regiões: América do Norte e América Latina)");
 
     // SVG
     // include the SVG and nested g element in which to plot the visualization
@@ -204,179 +229,5 @@ function ready(data) {
         .attr("width", (d, i) => licensesXScale(d.value));
 
 
-
-    // VERTICAL BAR CHART
-    // include variables which are used in stead of the margin.left, margin bottom and height defined for the other SVG
-    // the remainder of the margins and width values are used as-is
-    const marginLeftAge = margin.left / 2;
-    const marginBottomAge = margin.bottom + 20;
-    const heightAge = height / 2;
-
-    // replicate the reasoning applied to the first visualization, inclluding a wrapping section and introductory HTML elements
-    const age = container
-        .append("section");
-
-    age
-        .append("h2")
-        .text("Demography");
-
-    age
-        .append("p")
-        .text("More than half of hunters is 55 or older");
-
-    // include the data referring to the age intervals and percentages
-    // strucure the data in an array of objects, similarly to the first viz
-    const dataAge = [
-        {
-            category: '12-24',
-            value: 5.136755751
-        },
-        {
-            category: '25 - 34',
-            value: 9.855144098
-        },
-        {
-            category: '35 - 44',
-            value: 12.86331325
-        },
-        {
-            category: '45 - 54',
-            value: 19.03563665
-        },
-        {
-            category: '55 - 64',
-            value: 24.2365838
-        },
-        {
-            category: '65 - 74',
-            value: 19.08305805
-        },
-        {
-            category: '75 +',
-            value: 9.789508398
-        }
-    ];
-
-
-    // SVG
-    // using the updated values for the left margin and height values
-    const ageSVG = age
-        .append("svg")
-        .attr("viewBox", `0 0 ${width + marginLeftAge + margin.right} ${heightAge + margin.top + marginBottomAge}`)
-        .append("g")
-        .attr("transform", `translate(${marginLeftAge}, ${margin.top})`);
-
-    // SCALES
-    // switching the reasoning applied to the previous viz, as to have the bars drawn vertically
-
-    const ageYScale = d3
-        .scaleLinear()
-        // domain to go from 0 to 50 (as from 0 to 100 would render miniscule bars)
-        .domain([0, 50])
-        // range from the height to 0 to complement the top to bottom SVG logic
-        .range([heightAge, 0]);
-
-    const ageXScale = d3
-        .scaleBand()
-        .domain(dataAge.map(dataLicense => dataLicense.category))
-        .range([0, width]);
-
-    // AXES
-    const ageXAxis = d3
-        .axisBottom(ageXScale);
-
-    const ageYAxis = d3
-        .axisLeft(ageYScale)
-        // reduce the number of ticks
-        .ticks(5)
-        // format the vertical tick labels to show a percentage (expect for the origin, shown as-is)
-        .tickFormat((d) => (d !== 0) ? `${d}%` : `${d}`);
-
-    ageSVG
-        .append("g")
-        .attr("class", `axis`)
-        .attr("id", `x-axis`)
-        .attr("transform", `translate(0, ${heightAge})`)
-        .call(ageXAxis)
-        // select the tick labels and rotate them
-        .selectAll("text")
-        // move the labels to the left and to the bottom before rotating them (as the rotation hinges in the top left corner)
-        .attr("transform", "translate(-10, 10) rotate(-45)");
-
-    ageSVG
-        .append("g")
-        .attr("id", `y-axis`)
-        .attr("class", `axis`)
-        .call(ageYAxis);
-
-    // GRID LINES
-    // include grid lines, on the basis of horizontal ticks
-    ageSVG
-        .select("g#y-axis")
-        .selectAll("g.tick")
-        .append("line")
-        .attr("x1", 0)
-        .attr("y1", 0)
-        .attr("x2", width)
-        .attr("y2", 0)
-        .style("opacity", 0.3);
-
-    // FORMAT
-    // formatting function to have round numbers
-    const formatPercent = d3.format("d");
-
-    // VERTICAL BARS
-    // append a rect element for each data point
-    ageSVG
-        .selectAll("rect")
-        .data(dataAge)
-        .enter()
-        .append("rect")
-        // on hover show the tooltip with information regarding the category and the actual number of licenses
-        .on("mouseenter", (d) => {
-            console.log(d3.event);
-            tooltip
-                .style("opacity", 1)
-                .style("left", `${d3.event.pageX + 10}px`)
-                .style("top", `${d3.event.pageY - 10}px`);
-            tooltip
-                .select("p.title")
-                .text(() => `Age ${d.category}`);
-            tooltip
-                .select("p.description")
-                .text(() => `${formatPercent(d.value)}% of population`);
-        })
-        .on("mouseout", () => tooltip.style("opacity", 0))
-        // add a class of .bar to all rectangle elements
-        .attr("class", "bar")
-        // position the rectangle elements alongside the horizontal axis
-        .attr("x", (d) => ageXScale(d.category) + ageXScale.bandwidth() / 4)
-        .attr("width", ageXScale.bandwidth() / 2)
-        // include y and height values pushing the rectangle elements to the bottom of the SVG
-        // transition the bars by incresing the height value and moving the y coordinate where the bar would end (as SVG are drawn top to bottom)
-        .attr("y", ageYScale(0))
-        .attr("height", 0)
-        .transition()
-        .duration((d, i) => 2000 - 100 * i)
-        .delay((d, i) => 1800 + 100 * i)
-        .attr("y", (d) => ageYScale(d.value))
-        .attr("height", (d) => (heightAge) - ageYScale(d.value));
-
-}
-
-function findCountryContinent(country_name){
-    for (var i = 0, len = countryMapping.length; i < len; i++) {
-        if (countryMapping[i]["country"] === country_name){
-            if(countryMapping[i]["continent"] == "Americas"){
-                if(countryMapping[i]["sub_region"] == "Northern America"){
-                    return countryMapping[i]["sub_region"];
-                }
-                else{
-                    return countryMapping[i]["continent"];
-                }
-            } 
-            return countryMapping[i]["continent"];
-        }
-    }
 }
 
